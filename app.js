4069794213b1e6841601231d37e8f96fe0dac881
lyju777 <lyju777@hanmail.net>
2023-09-06 21:34:50 +0900

    Events 적용

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick(){
-    console.dir(loginInput.value);
+const link = document.querySelector("a");
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click" , onLoginBtnClick)
+function handleLinkClick(event) {
+  event.preventDefault();
+  alert("clicked!");
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
