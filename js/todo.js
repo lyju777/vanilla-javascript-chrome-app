04fe2d2708723efe3f3f977611e00cb13ac946ad
lyju777 <lyju777@hanmail.net>
2023-09-10 16:49:04 +0900

    background/quotes/todo~ing

---
+++ js/todo.js
@@ -0,0 +1,38 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-List");
+
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem("toDos", JSON.stringify(toDos));
+}
+
+function deleteTodo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteTodo);
+  li.appendChild(span);
+  li.appendChild(button);
+
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  toDos.push(newTodo);
+  paintToDo(newTodo);
+  saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
